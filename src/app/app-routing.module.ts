f4402bccd0ae4dd17978c56747727ff647875947
DevoRia <the.rocky.vadim@gmail.com>
2020-10-24 10:39:26 +0300

    add(routing)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewPageComponent } from './pages/view-page/view-page.component';
+import { DetailsPageComponent } from './pages/details-page/details-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'articles', pathMatch: 'full' },
+  { path: 'articles', component: ViewPageComponent },
+  { path: 'news', component: ViewPageComponent },
+  { path: 'details/:id/articles', component: DetailsPageComponent },
+  { path: 'details/:id/news', component: DetailsPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
